e4cf4d24185d95d1092b8a8f4a4d0a303ac36a1f
Manju L <manjugowda200523@gmail.com>
2026-01-22 12:50:40 +0530

    Find  Maximum in 2D

---
+++ src/com/manju/LinearSearch/MaxIn2D.java
@@ -0,0 +1,26 @@
+package com.manju.LinearSearch;
+
+public class MaxIn2D {
+    static void main() {
+        int[][] arr={
+                {14,12,8},
+                {18,12,3,9},
+                {54,24},
+                {18,52,42,14}
+        };
+
+        System.out.println(max(arr));
+
+    }
+    static int max(int[][] arr){
+        int maxVal=Integer.MIN_VALUE;
+        for(int i=0;i< arr.length;i++){
+            for(int j=0;j<arr[i].length;j++){
+                if(arr[i][j]>maxVal){
+                    maxVal=arr[i][j];
+                }
+            }
+        }
+        return maxVal;
+    }
+}
