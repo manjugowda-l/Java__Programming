b20eb395ad7e64a779445f96715e21f60d4d6b2c
Manju L <manjugowda200523@gmail.com>
2026-01-23 07:31:38 +0530

    Ceiling of a number

---
+++ src/com/manju/BinarySearch/CeilingOfNumber.java
@@ -0,0 +1,41 @@
+package com.manju.BinarySearch;
+
+public class CeilingOfNumber {
+    static void main() {
+        int[] arr={-18,-12,-6,0,12,14,23,52,67,89};
+        int target=14;
+        int ans=OrderAgnosticBS(arr,target);
+
+        System.out.println("Smallest value greater than or equal to target is  " + ans);
+
+    }
+    static int OrderAgnosticBS(int[] arr,int target){
+        int start=0;
+        int end=arr.length-1;
+        boolean isAsc =arr[start]<arr[end];
+
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(arr[mid]==target){
+                return arr[mid];
+            }
+            if(isAsc){
+                if(target<arr[mid]){
+                    end=mid-1;
+
+                }else if (target>arr[mid]) {
+                    start = mid + 1;
+                }
+
+            }else{
+                if(target>arr[mid]){
+                    end=mid-1;
+
+                }else if (target<arr[mid]) {
+                    start = mid + 1;
+                }
+            }
+        }
+        return arr[start];
+    }
+}
