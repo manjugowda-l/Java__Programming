f77c7bda9ede5fde728f4a88e68008ef93cbc1b5
Manju L <manjugowda200523@gmail.com>
2026-01-26 20:21:30 +0530

    Row and Col wise sorted

--- src/com/manju/BinarySearch/RowAndColumnWiseSorted.java
+++ src/com/manju/BinarySearch/RowAndColumnWiseSorted.java
@@ -2,7 +2,7 @@ package com.manju.BinarySearch;
 
 import java.util.Arrays;
 
-public class BSIn2D {
+public class RowAndColumnWiseSorted {
     static void main() {
         int[][] arr={
                 {10,20,30,40},
