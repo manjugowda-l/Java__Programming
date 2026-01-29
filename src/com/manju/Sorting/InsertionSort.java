dee43dd448f85dd14501de2740eae7d9bdd939dd
Manju L <manjugowda200523@gmail.com>
2026-01-29 06:21:39 +0530

    Insertion Sort

---
+++ src/com/manju/Sorting/InsertionSort.java
@@ -0,0 +1,33 @@
+package com.manju.Sorting;
+
+import java.util.Arrays;
+
+public class InsertionSort {
+    static void main() {
+        int[] arr={5,4,3,2,1};
+        insertion(arr);
+        System.out.println(Arrays.toString(arr));
+
+    }
+
+    static void insertion(int[] arr){
+        for(int i=0;i< arr.length-1;i++){
+            for(int j=i+1;j>0;j--){
+                if(arr[j]<arr[j-1]){
+                    swap(arr,j,j-1);
+                }else{
+                    break;
+                }
+            }
+
+        }
+    }
+
+    static void swap(int[] arr,int first,int second){
+        int temp=arr[first];
+        arr[first]=arr[second];
+        arr[second]=temp;
+    }
+
+
+}
