b7c7cdd607fbd57e68a970aac337b420b7f2d582
Manju L <manjugowda200523@gmail.com>
2026-01-30 12:21:51 +0530

    Pretty Printing

---
+++ src/com/manju/String/PrettyPrinting.java
@@ -0,0 +1,10 @@
+package com.manju.String;
+
+public class PrettyPrinting {
+    static void main() {
+        float a=456.1234f;
+//        System.out.printf("Formatted number is %.2f" ,a);
+        System.out.printf("Pi %.3f", Math.PI);
+        System.out.printf("Hello my name is  %s and I am %s ","ManjuGowda","Cool");
+    }
+}
