efb47cf88167633c3e44262b29a7227669fa4254
Manju L <manjugowda200523@gmail.com>
2026-01-30 11:07:25 +0530

    String Comparison

---
+++ src/com/manju/String/Comparison.java
@@ -0,0 +1,9 @@
+package com.manju.String;
+
+public class Comparison {
+    public static void main(String[] args) {
+        String a="Manju";
+        String b="Manju";
+        System.out.println(a==b);
+    }
+}
