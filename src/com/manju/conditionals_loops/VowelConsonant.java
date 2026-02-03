4b2f339c4231386fb754614c354bb16317112d47
Manju L <manjugowda200523@gmail.com>
2026-02-03 16:23:20 +0530

    Vowel and consonent

---
+++ src/com/manju/conditionals_loops/VowelConsonant.java
@@ -0,0 +1,17 @@
+package com.manju.conditionals_loops;
+
+import java.util.Scanner;
+
+public class VowelConsonant {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        char ch = in.next().charAt(0);
+
+        if (ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||
+                ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U') {
+            System.out.println("Vowel");
+        } else {
+            System.out.println("Consonant");
+        }
+    }
+}
