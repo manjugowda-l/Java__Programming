afc1c07f65d669a21926e712527d339d8962dff9
Manju L <manjugowda200523@gmail.com>
2026-02-06 17:02:26 +0530

    Recursion Fibonacci

---
+++ src/com/manju/Recursion/Fibonacci.java
@@ -0,0 +1,15 @@
+package com.manju.Recursion;
+
+public class Fibonacci {
+    static void main() {
+        System.out.println(fibo(7));
+
+    }
+    static int fibo(int n){
+        if(n<2){
+            return n;
+        }
+        return fibo(n-1)+fibo(n-2);
+
+    }
+}
