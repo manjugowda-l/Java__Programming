8d5def0a6acac09d6f279b1000da7369ed9d5edd
Manju L <manjugowda200523@gmail.com>
2026-02-08 23:20:48 +0530

    Dominant No

---
+++ src/com/manju/Bitwise/DominantNo.java
@@ -0,0 +1,16 @@
+package com.manju.Bitwise;
+
+public class DominantNo {
+    static void main() {
+        int[] arr={3,2,4,5,4,3,2};
+        System.out.println(ans(arr));
+
+    }
+    static int ans(int[] arr){
+        int unique=0;
+        for (int n:arr){
+            unique^=n;
+        }
+        return unique;
+    }
+}
