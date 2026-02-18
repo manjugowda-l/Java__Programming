6bb3af8a7f1e8746ca946433f60cc0e3e4b278e2
Manju L <manjugowda200523@gmail.com>
2026-02-18 20:31:16 +0530

    REcusive Factorial

---
+++ src/com/manju/Recursion/Factorial.java
@@ -0,0 +1,16 @@
+package com.manju.Recursion;
+
+public class Factorial {
+    static void main() {
+        System.out.println(fact(5));
+
+    }
+    static int fact(int n){
+        if(n==1){
+            return 1;
+        }
+        return (n*fact(n-1));
+
+
+    }
+}
