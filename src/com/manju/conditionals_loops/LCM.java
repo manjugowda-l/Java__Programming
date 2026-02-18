5dea3ec7c051358945713503a8a248e0c6dede74
Manju L <manjugowda200523@gmail.com>
2026-02-18 16:21:02 +0530

    LCM

---
+++ src/com/manju/conditionals_loops/LCM.java
@@ -0,0 +1,17 @@
+package com.manju.conditionals_loops;
+
+public class LCM {
+    static void main() {
+        System.out.println(gcd(105,224));
+    }
+    static int gcd(int a,int b){
+        if(a==0){
+            return b;
+        }
+        return gcd(b%a,a);
+    }
+    static int lcm(int a ,int b){
+        return a*b/gcd(a,b);
+
+    }
+}
