c2033babcb8e50ea461feea9a6ce01d675160c1b
Manju L <manjugowda200523@gmail.com>
2026-02-20 19:17:37 +0530

    Merge Sort

---
+++ src/com/manju/Recursion_On_Sort/Merge_Sort.java
@@ -0,0 +1,50 @@
+package com.manju.Recursion_On_Sort;
+
+import java.util.Arrays;
+
+public class Merge_Sort {
+    static void main() {
+        int[] arr ={8,5,6,4,3,1};
+        int[] ans= mergeSort(arr);
+        System.out.println(Arrays.toString(ans));
+
+
+    }
+    static int[] mergeSort(int[] arr){
+        if(arr.length==1){
+            return arr;
+        }
+        int mid=arr.length/2;
+        int[] left =mergeSort(Arrays.copyOfRange(arr,0,mid));
+        int[] right =mergeSort(Arrays.copyOfRange(arr,mid,arr.length));
+        return merge(left,right);
+    }
+    private static int[] merge(int[] first,int[] second){
+        int[] mix = new int[first.length+second.length];
+        int i=0;
+        int j=0;
+        int k=0;
+        while(i<first.length&&j<second.length){
+            if(first[i]<second[j]){
+                mix[k]=first[i];
+                i++;
+            }else{
+                mix[k]=second[j];
+                j++;
+            }
+            k++;
+        }
+        while(i<first.length){
+            mix[k]=first[i];
+            i++;
+            k++;
+
+        }
+        while(j<second.length){
+            mix[k]=second[j];
+            j++;
+            k++;
+        }
+        return mix;
+    }
+}
