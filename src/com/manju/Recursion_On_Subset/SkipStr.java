a7afa0b24fd0722a78d5a26ec865c69af8a43fc5
Manju L <manjugowda200523@gmail.com>
2026-02-23 18:04:16 +0530

    Skip a String

---
+++ src/com/manju/Recursion_On_Subset/SkipStr.java
@@ -0,0 +1,20 @@
+package com.manju.Recursion_On_Subset;
+
+public class SkipStr {
+    static void main() {
+        System.out.println(skip("bccapplesda"));
+
+    }
+    static String skip(String up){
+        if(up.isEmpty()){
+            return " ";
+        }
+
+        if (up.startsWith("apple")){
+            return skip(up.substring(5));
+        }else{
+            return up.charAt(0)+ skip(up.substring(1));
+        }
+    }
+
+}
