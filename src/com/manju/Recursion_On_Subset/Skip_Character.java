518281bfa59f9074fe2ad9d4e9243d31eacfc115
Manju L <manjugowda200523@gmail.com>
2026-02-23 17:47:05 +0530

    Skip a Character

---
+++ src/com/manju/Recursion_On_Subset/Skip_Character.java
@@ -0,0 +1,21 @@
+package com.manju.Recursion_On_Subset;
+
+public class Skip_Character {
+    static void main() {
+        skip(" ","bccasda");
+
+    }
+    static void skip(String p,String up){
+        if(up.isEmpty()){
+            System.out.println(p);
+            return;
+        }
+        char ch=up.charAt(0);
+        if(ch=='a'){
+            skip(p,up.substring(1));
+
+        }else{
+            skip(p+ch,up.substring(1));
+        }
+    }
+}
