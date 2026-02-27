96adf1f1492b59b8c37a46ce8d954f8650a399d1
Manju L <manjugowda200523@gmail.com>
2026-02-27 05:45:04 +0530

    Subsequences

--- src/com/manju/Recursion_On_Subset/Subseq.java
+++ src/com/manju/Recursion_On_Subset/Subseq.java
@@ -1,8 +1,11 @@
 package com.manju.Recursion_On_Subset;
 
+import java.util.ArrayList;
+
 public class Subseq {
     static void main() {
-        subseq("","abc");
+//        subseq("","abc");
+        System.out.println(subseqRet("","abc"));
     }
 
     static void subseq(String p,String up){
@@ -13,5 +16,18 @@ public class Subseq {
         char ch =up.charAt(0);
         subseq(p+ch,up.substring(1));
         subseq(p,up.substring(1));
+    }
+
+    static ArrayList<String> subseqRet(String p, String up){
+        if(up.isEmpty()){
+            ArrayList<String> list=new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+        char ch =up.charAt(0);
+        ArrayList<String> left=subseqRet(p+ch,up.substring(1));
+        ArrayList<String> right=subseqRet(p,up.substring(1));
+        left.addAll(right);
+        return left;
     }
 }
