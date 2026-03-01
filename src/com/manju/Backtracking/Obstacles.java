531dc47c76c554b031ca562a5a79fbfa98995a29
Manju L <manjugowda200523@gmail.com>
2026-03-02 04:58:03 +0530

    Maze with Obstacles

---
+++ src/com/manju/Backtracking/Obstacles.java
@@ -0,0 +1,28 @@
+package com.manju.Backtracking;
+
+public class Obstacles {
+    static void main() {
+        boolean[][] board={
+                {true,true,true},
+                {true,false,true},
+                {true,true,true}
+        };
+        path("",board,0,0);
+    }
+    static void path(String p,boolean[][] maze,int r,int c){
+        if(r==maze.length-1&&c==maze[0].length-1){
+            System.out.println(p);
+            return;
+        }
+        if(!maze[r][c]){
+            return;
+        }
+        if(r<maze.length-1) {
+
+            path(p + 'D', maze,r + 1, c);
+        }
+        if(c<maze[0].length-1){
+            path(p + 'R', maze,r, c + 1);
+        }
+    }
+}
