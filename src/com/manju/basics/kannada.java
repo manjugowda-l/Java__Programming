6db99ce14065fc16c5a879af3183279b0b43cbfc
Manju L <manjugowda200523@gmail.com>
2026-05-19 06:58:48 +0530

    REvision

--- src/com/manju/basics/kannada.java
+++ src/com/manju/basics/kannada.java
@@ -2,6 +2,16 @@ package com.manju.basics;
 
 public class kannada {
     public static void main(String[] args) {
-        System.out.println("ನಮಸ್ಕಾರ");
+        int reverse=12345;
+        int ans =0;
+        while(reverse>0){
+            int rem=reverse%10;
+
+            ans =ans*10+rem;
+            reverse /=10;
+
+
+        }
+        System.out.println(ans);
     }
 }
